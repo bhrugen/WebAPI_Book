5c14047379d10365bf954cb2c7f6d8b6c6dcc1bc
Ben Spark <bhrugen1990@gmail.com>
2017-12-12 20:18:48 -0600

    Controller

---
+++ WebAPI_Book/WebAPI_Book/Controllers/BookController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI_Book.Models;
+
+namespace WebAPI_Book.Controllers
+{
+    public class BookController : ApiController
+    {
+
+        Book[] books = new Book[]
+            {
+                new Book(){ Id=1,Author="Billy Goat",Price=100,Title="Spider without Duty"},
+                new Book(){ Id=2,Author="Happy Sheep",Price=200,Title="Giant and Theives"},
+                new Book(){ Id=3,Author="Jimmy Bear",Price=300,Title="Fortune of Time"},
+            };
+
+        // GET: api/Book
+        public IHttpActionResult Get()
+        {
+            return Ok(books);
+        }
+
+        // GET: api/Book/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Book
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Book/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Book/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
