dbd5ebd1390fe7336c3165dfab1e3b1fc86e871b
Ben Spark <bhrugen1990@gmail.com>
2017-12-12 20:16:56 -0600

    Model

---
+++ WebAPI_Book/WebAPI_Book/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI_Book.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public double Price { get; set; }
+    }
+}
